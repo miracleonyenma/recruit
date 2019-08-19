f944fa2ff475013ce0e59983674af3a89303ea9c
miracleonyenma <miracleficient@gmail.com>
2019-08-19 03:57:31 +0100

    Trying to fix the previous button functionalty

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var sections = document.querySelectorAll(".form-sect"),
+    prevBtn = document.querySelector("#prev"),
     nextBtn = document.querySelector("#next"),
+    cntwrpr = document.querySelector(".content-wrapper"),
+    crntPos,
     pos = 0,
     count = sections.length,
     i = 0,
@@ -25,24 +28,26 @@ function nextStep(){
     });
     nextTl.add({
         targets: '.blinds',
+        // translateY: '0px',
         height: 0
     });
     
     if(pos < count - 1 ){
         displacement = sections[pos].clientHeight;
-        
+
         nextTl.add({
             targets: sections[pos],
             opacity: 0
         }, "-=600");
         nextTl.add({
             targets: '.content-wrapper',
-            translateY: -displacement + 'px'
+            translateY: -displacement * (pos + 1) + 'px'
         }, "-=600");
         nextTl.add({
             targets: sections[pos + 1],
             opacity: 1
         }, "-=600");
+        crntPos = -displacement * (pos + 1);
         pos++;
         console.log(pos);
 
@@ -51,9 +56,52 @@ function nextStep(){
     else{
         pos = 0;
     }
+    crntPos = parseInt(window.getComputedStyle(cntwrpr).transform.split(" ")[5].split(")")[0]);
 
 }
 
-nextBtn.addEventListener("click", nextStep);
+function prevStep(){
+    var prevTl = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 750,
+        autoplay: false
+    });
+    prevTl.add({
+        targets: '.blinds',
+        backgroundColor: "#2771f3"
+    });
+    prevTl.add({
+        targets: '.blinds',
+        height: "100%"
+    });
+    prevTl.add({
+        targets: '.blinds',
+        translateY: '100%',
+        scaleY: 0
+    })
 
+    if(pos > 0){
+        displacement = sections[pos].clientHeight;
+        pos--;
+        console.log(pos);
+        prevTl.add({
+            targets: '.content-wrapper',
+            translateY: crntPos + (displacement) + 'px'
+        });
+        prevTl.add({
+            targets: sections[pos],
+            opacity: 1
+        });
+
+        prevTl.play();
+
+    }
+
+    else{
+        console.log('else' + pos);
+    }
+}
+
+nextBtn.addEventListener("click", nextStep);
+prevBtn.addEventListener("click", prevStep);
 window.addEventListener("load", resetState);
