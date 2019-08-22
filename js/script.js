201514580b7cc6098476050c9c46509a32335d96
miracleonyenma <miracleficient@gmail.com>
2019-08-22 01:49:38 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ var sections = document.querySelectorAll(".sect-wrapper"),
     nextBtn = document.querySelector("#next"),
     cntwrpr = document.querySelector(".content-wrapper"),
     form = document.querySelector("#form"),
-    styles = document.documentElement.style,
+    styles = document.documentElement.style, 
+    // submitTl = submitTl2 = anime.timeline({
+    //     targets: '.controls-cont.submit',
+    //     easing: 'easeOutExpo',
+    //     duration: 800,
+    //     autoplay: false
+    // }),
     formHeight,
     crntPos,
     pos = 0,
@@ -30,35 +36,35 @@ function nextStep(){
 
     var nextTl = anime.timeline({
         easing: 'easeOutExpo',
-        duration: 750,
+        duration: 800,
         autoplay: false
     });
     
-    nextTl.add({
-        targets: '.blinds',
-        backgroundColor: "#2771f3"
-    });
-    nextTl.add({
-        targets: '.blinds',
-        // translateY: '0px',
-        height: 0
-    });
+    // nextTl.add({
+    //     targets: '.blinds',
+    //     backgroundColor: "#2771f3"
+    // });
+    // nextTl.add({
+    //     targets: '.blinds',
+    //     // translateY: '0px',
+    //     height: 0
+    // });
     
     if(pos < count - 1 ){
         displacement = sections[pos + 1].clientHeight;
 
         nextTl.add({
-            targets: sections[pos].children[0],
-            opacity: 0
-        }, "-=600");
-        nextTl.add({
             targets: '.content-wrapper',
             translateY: -displacement * (pos + 1) + 'px'
-        }, "-=600");
+        });
+        nextTl.add({
+            targets: sections[pos].children[0],
+            opacity: 0
+        }, "-=800");
         nextTl.add({
             targets: sections[pos + 1].children[0],
             opacity: 1
-        }, "-=600");
+        }, "-=400");
         crntPos = -displacement * (pos + 1);
         pos++;
         console.log(pos);
@@ -68,44 +74,54 @@ function nextStep(){
     else{
         // pos = 0;
     }
+
+    if(pos == sections.length - 1){
+        anime({
+            targets: '.controls-cont.submit',
+            easing: 'easeOutExpo',
+            opacity: 1,
+            duration: 800,
+                top: "80%"
+        });
+    }
 }
 
 function prevStep(){
     var prevTl = anime.timeline({
         easing: 'easeOutExpo',
-        duration: 750,
+        duration: 800,
         autoplay: false
     });
-    prevTl.add({
-        targets: '.blinds',
-        backgroundColor: "#2771f3"
-    });
-    prevTl.add({
-        targets: '.blinds',
-        height: "100%"
-    });
-    prevTl.add({
-        targets: '.blinds',
-        translateY: '100%',
-        scaleY: 0
-    })
+    // prevTl.add({
+    //     targets: '.blinds',
+    //     backgroundColor: "#2771f3"
+    // });
+    // prevTl.add({
+    //     targets: '.blinds',
+    //     height: "100%"
+    // });
+    // prevTl.add({
+    //     targets: '.blinds',
+    //     translateY: '100%',
+    //     scaleY: 0
+    // })
 
     if(pos > 0){
         displacement = sections[pos].clientHeight;
         console.log(pos);
         pos--;
         prevTl.add({
-            targets: sections[pos].children[0],
-            opacity: 1
-        }, "-=600");
-        prevTl.add({
             targets: '.content-wrapper',
             translateY: crntPos + (displacement) + 'px'
-        }, "-=600");
+        });
+        prevTl.add({
+            targets: sections[pos].children[0],
+            opacity: 1
+        }, "-=400");
         prevTl.add({
             targets: sections[pos + 1].children[0],
             opacity: 0
-        }, "-=600");
+        },"-=700");
 
         prevTl.play();
     }
@@ -113,6 +129,17 @@ function prevStep(){
     else{
         console.log('else' + pos);
     }
+
+    if(pos < sections.length - 1){
+        anime({
+            targets: '.controls-cont.submit',
+            easing: 'easeOutExpo',
+            duration: 800,
+            opacity: 0,
+            top: "100%"
+        });
+    }
+
 }
 
 nextBtn.addEventListener("click", nextStep);
